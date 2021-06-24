f1e958de593dfb70e5af1bec84a0098c80a6b8d9
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-06-24 17:27:45 +0530

    minor changes
    Signed-off-by: Saurav Khdoolia <sauravkhdoolia@gmail.com>

--- layouts/blog.js
+++ layouts/blog.js
@@ -1,11 +1,10 @@
 // const editUrl = (slug) =>
-//   `https://github.com/leerob/leerob.io/edit/main/data/blog/${slug}.mdx`;
+//   `https://github.com/ksrov/blog/edit/main/data/blog/${slug}.mdx`;
 // const discussUrl = (slug) =>
 //   `https://mobile.twitter.com/search?q=${encodeURIComponent(
-//     `https://leerob.io/blog/${slug}`
+//     `https://sauravkhdoolia.com/blog/${slug}`
 //   )}`;
 
-export default function BlogLayout({ children, frontMatter }) {
-  console.log(frontMatter);
+export default function BlogLayout({ children }) {
   return <div>{children}</div>;
 }
