385b73f15e37ea0a725d2dbb4f4b2c6eec24dec7
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-06-24 21:32:51 +0530

    themes are here
    Signed-off-by: Saurav Khdoolia <sauravkhdoolia@gmail.com>

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,7 +13,7 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
           />
         </Head>
-        <body className="w-full h-full bg-white dark:bg-black drak:text-white text-black">
+        <body className="w-full h-full bg-white dark:bg-gray-800 drak:text-white text-black">
           <Main />
           <NextScript />
         </body>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,19 @@
 import Link from 'next/link';
+import { Moon, Sun } from 'react-feather';
+import { useTheme } from 'next-themes';
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
+
+  const isLight = theme === 'light';
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full ">
+    <div className="flex flex-col items-center justify-center w-full h-full relative">
+      <button
+        className="absolute top-4 right-4 bg-gray-300 dark:bg-gray-600 p-2 rounded border border-transparent dark:hover:border-gray-500 hover:border-gray-400 cursor-pointer"
+        onClick={() => setTheme(isLight ? 'dark' : 'light')}
+      >
+        {isLight ? <Moon size={18} /> : <Sun size={18} />}
+      </button>
       <h1 className="text-2xl mb-4 text-black dark:text-white text-center">
         I&#39;m being worked on...
       </h1>
