26a0e3adcd09b5406ed77ce817dbb9fa6d577087
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-06-28 08:39:27 +0530

    Add Layout to _app and Providers
    Signed-off-by: Saurav Khdoolia <sauravkhdoolia@gmail.com>

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,13 +2,35 @@ import 'styles/global.css';
 
 import { ThemeProvider } from 'next-themes';
 import { MDXProvider } from '@mdx-js/react';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import MDXComponents from 'components/MDXComponents';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+import { defaultQueryFn } from '../lib/fetcher';
+
+// Create a client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: defaultQueryFn
+    }
+  }
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <MDXProvider components={MDXComponents}>
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <RecoilRoot>
+            <Header />
+            <main className="w-full max-w-2xl mx-auto main-content">
+              <Component {...pageProps} />
+            </main>
+            <Footer />
+          </RecoilRoot>
+        </QueryClientProvider>
       </MDXProvider>
     </ThemeProvider>
   );
