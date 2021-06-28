9337b16c46a39aac534ae500e5cf292d00230d90
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-06-28 08:41:28 +0530

    woops add defaultQueryFn to storybook
    Signed-off-by: Saurav Khdoolia <sauravkhdoolia@gmail.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,9 +3,16 @@ import { addDecorator } from '@storybook/react'
 import { RecoilRoot } from 'recoil';
 import {QueryClient, QueryClientProvider} from 'react-query'
 import '@storybook/addon-console';
+import {defaultQueryFn} from '../lib/fetcher'
 import "../styles/global.css"
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: defaultQueryFn
+    }
+  }
+})
 
 const Providers = ({children}) => <> 
   <div className="max-w-4xl max-h-content flex items-center justify-center">
