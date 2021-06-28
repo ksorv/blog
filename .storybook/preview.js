957899e2b9ca705ff20a83f1deefd7a30c7a3f23
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-06-28 08:41:03 +0530

    add QueryProvider and defaultQUery fn
    Signed-off-by: Saurav Khdoolia <sauravkhdoolia@gmail.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react'
 import { RecoilRoot } from 'recoil';
+import {QueryClient, QueryClientProvider} from 'react-query'
 import '@storybook/addon-console';
 import "../styles/global.css"
 
+const queryClient = new QueryClient()
+
 const Providers = ({children}) => <> 
   <div className="max-w-4xl max-h-content flex items-center justify-center">
+    <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         {children}
       </RecoilRoot>
+    </QueryClientProvider>
   </div>
 </>
 
