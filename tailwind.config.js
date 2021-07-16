4d66fb25bb48b9d482d25c3ef27263369d075267
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-07-16 19:30:32 +0530

    removed jsx files from tailwind config
    Signed-off-by: Saurav Khdoolia <sauravkhdoolia@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: [
-    './pages/**/*.jsx',
-    './components/**/*.jsx',
-    './layouts/**/*.jsx',
-    './pages/**/*.tsx',
-    './components/**/*.tsx',
-    './layouts/**/*.tsx'
-  ],
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx', './layouts/**/*.tsx'],
   darkMode: 'class',
   theme: {
     extend: {
