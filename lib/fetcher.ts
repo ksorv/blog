76a3e6472bea0cfdc2605890fcedc86e6c7c7958
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-12-17 10:16:18 +0530

    remix changes
    Signed-off-by: Saurav Khdoolia <sauravkhdoolia@gmail.com>

--- lib/fetcher.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import { QueryFunction } from 'react-query';
-
-export const defaultQueryFn: QueryFunction = async ({ queryKey }) => {
-  const url = `/api${queryKey[0]}`;
-  const { data } = await axios.get(url);
-  return data;
-};
