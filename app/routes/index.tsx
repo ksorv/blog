beefd0ec65e00a7f3ca7c1146e0a0685a4721aa2
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-12-22 20:47:18 +0530

    enhance: remove redundant code
    Signed-off-by: Saurav Khdoolia <sauravkhdoolia@gmail.com>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Link, MetaFunction, useCatch } from 'remix';
+import { MetaFunction, useCatch } from 'remix';
 
 export const meta: MetaFunction = () => {
   return {
@@ -12,20 +12,6 @@ export default function Index() {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center text-primary/100">
       Being rebuilt with Remix, TypeScript, Netlify & Tailwind(probably).
-      <div className="flex my-6 items-center justify-center gap-4 text-primary">
-        <Link
-          to="/colors"
-          className="text-tertiary underline underline-offset-1"
-        >
-          Colors
-        </Link>
-        <Link
-          to="/typography"
-          className="text-secondary underline underline-offset-1"
-        >
-          Typography
-        </Link>
-      </div>
     </div>
   );
 }
