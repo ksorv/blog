d83bdd78950963d5c409beebf4c6b3743c3f08da
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-12-22 21:32:09 +0530

    enhance: remove netlify plugin as build failed due to a pkg not being there
    Signed-off-by: Saurav Khdoolia <sauravkhdoolia@gmail.com>

--- netlify/plugins/tailwind-helper/index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-
-module.exports = {
-  onPreBuild: async ({ utils: { run } }) => {
-    await run.command('npm run build:tailwind');
-    console.log('%c tailwind build complete', 'color: cyan');
-  }
-};
