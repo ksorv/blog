c4008d40db990353e6715fbe841c91da5aa262b1
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-12-22 20:06:57 +0530

    enhan: extend colors instead of replacing them for more freedom and breakpoint changes
    Signed-off-by: Saurav Khdoolia <sauravkhdoolia@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,33 +2,34 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 const getColor =
   (colorCode) =>
-  ({ opacityVariable, opacityValue }) =>
-    opacityVariable
-      ? `rgba(var(--${colorCode}) / var(${opacityVariable}))`
-      : opacityValue
-      ? `rgba(var(--${colorCode}) / ${opacityValue})`
-      : `rgba(var(--${colorCode}) / 1)`;
+    ({ opacityVariable, opacityValue }) =>
+      opacityVariable
+        ? `rgba(var(--${colorCode}) / var(${opacityVariable}))`
+        : opacityValue
+          ? `rgba(var(--${colorCode}) / ${opacityValue})`
+          : `rgba(var(--${colorCode}) / 1)`;
 
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   darkMode: 'class',
   content: ['./app/**/*.tsx'],
   theme: {
-    colors: {
-      primary: getColor('primary'),
-      'primary-accent': getColor('primary-accent'),
-      'secondary-accent': getColor('secondary-accent')
-    },
     screens: {
-      md: '640px',
+      sm: '640px',
+      md: '768px',
       lg: '1024px',
-      xl: '1500px'
+      xl: '1440px',
     },
     extend: {
+      colors: {
+        primary: getColor('primary'),
+        'primary-accent': getColor('primary-accent'),
+        'secondary-accent': getColor('secondary-accent')
+      },
       fontFamily: {
         sans: ['Nunito', ...defaultTheme.fontFamily.sans]
       }
     }
   },
-  plugins: []
+  plugins: [require('@tailwindcss/typography')]
 };
