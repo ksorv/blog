1aeb2cea636df0f69030ebb13bf15be4d097873d
Saurav Kumar <sorv@ksorv.com>
2022-10-30 21:47:52 +0530

    again
    Signed-off-by: Saurav Kumar <sorv@ksorv.com>

--- remix.config.js
+++ remix.config.js
@@ -1,36 +1,15 @@
-const { getDependenciesToBundle } = require("@remix-run/dev");
-
-
-// console.log(getDependenciesToBundle('mdx-bundler'))
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   serverDependenciesToBundle: [
-    "bail",
-    "fault",
-    "zwitch",
-    "trough",
-    "unified",
-    "periscopic",
-    "mdx-bundler",
-    "parse-entities",
-    "longest-streak",
-    "stringify-entities",
-    "property-information",
-    "space-separated-tokens",
-    "comma-separated-tokens",
-    "decode-named-character-reference",
-    "@fal-works/esbuild-plugin-global-externals",
-    "/^is.*/",
-    "/^hast.*/",
-    "/^unist-*/",
-    "^/vfile.*/",
-    "/^mdast.*/",
-    "/^estree.*/",
-    "/^remark.*/",
-    "/^@mdx-js.*/",
-    "/^character.*/",
-    "/^micromark.*/",
+    'remark-gfm',
+    /micromark-.*/,
+    /mdast-.*/,
+    'ccount',
+    /unist-.*/,
+    'decode-named-character-reference',
+    'character-entities',
+    'markdown-table'
   ]
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
