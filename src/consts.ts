5101eaee81eaad9a0fd2db3f67af43ad9653a121
Saurav Kumar <sauravkhdoolia@gmail.com>
2024-07-09 23:27:19 +0530

    being worked upon

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'KSORV';
+export const SITE_DESCRIPTION = 'I build scalable frontends and try to write about things that I find interesting.';
---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		date: z.date(),
+		updatedDate: z.date().optional(),
+		heroImage: z.string().optional(),
+		tags: z.array(z.string())
+	}),
+});
+
+export const collections = { blog };
