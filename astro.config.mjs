dfcc0eeba1422160b878dca5169aa97765602ebe
Saurav Kumar <sauravkhdoolia@gmail.com>
2024-07-16 17:45:13 +0530

    tailwind, shacdn, and header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
-import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ksorv.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://ksorv.com",
+  integrations: [mdx(), sitemap(), tailwind({
+    applyBaseStyles: false
+  }), react()]
 });
